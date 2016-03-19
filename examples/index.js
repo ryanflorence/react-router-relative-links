1cf7f8a9897fe4141f8aaebd10fa7adc5d521a5a
Ryan Florence <rpflorence@gmail.com>
2016-03-18 22:01:56 -0700

    added example

---
+++ examples/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory, Link } from 'react-router'
+
+import {
+  createElement as relativeLinksCreateElement,
+  RelativeLink
+} from 'react-router-relative-links'
+
+const App = React.createClass({
+  render() {
+    return (
+      <div style={{ fontFamily: 'sans-serif', fontWeight: '200' }}>
+        <h1>Relative Links!</h1>
+        <div style={{ display: 'flex' }}>
+          <div>
+            <h2>Index</h2>
+            <ul>
+              <li> <Link to="/ch/1">Chapter 1</Link> </li>
+              <li> <Link to="/ch/2">Chapter 2</Link> </li>
+            </ul>
+          </div>
+          <div style={{ flex: '1', paddingLeft: '60px' }}>
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    )
+  }
+})
+
+const Home = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+      </div>
+    )
+  }
+})
+
+const Chapter = React.createClass({
+  render() {
+    const { chapter } = this.props.params
+    return (
+      <div>
+        <h2>Chapter {chapter}</h2>
+        <ul>
+          <li><RelativeLink to="../..">Up to Home</RelativeLink></li>
+          <li><RelativeLink to="./1">Chapter {chapter}, Page 1</RelativeLink></li>
+          <li><RelativeLink to="./2">Chapter {chapter}, Page 2</RelativeLink></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+const Page = React.createClass({
+  render() {
+    const { chapter, page } = this.props.params
+    return (
+      <div>
+        <h2>Chapter {chapter}, Page {page}</h2>
+        <ul>
+          <li><RelativeLink to={`../../..`}>Up to Home</RelativeLink></li>
+          <li><RelativeLink to={`..`}>Up to Chapter {chapter}</RelativeLink></li>
+        </ul>
+      </div>
+    )
+  }
+})
+
+render((
+  <Router
+    history={browserHistory}
+    createElement={relativeLinksCreateElement}
+  >
+    <Route path="/" component={App}>
+      <IndexRoute component={Home}/>
+      <Route path="ch/:chapter">
+        <IndexRoute component={Chapter}/>
+        <Route path=":page" component={Page}/>
+      </Route>
+    </Route>
+  </Router>
+), document.getElementById('app'))
+
---
+++ examples/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'index.js'),
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    alias: {
+      'react-router-relative-links':
+        path.resolve(__dirname, '..', 'modules', 'index.js')
+    }
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    quiet: false,
+    noInfo: false,
+    stats: {
+      assets: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: true
+    }
+  }
+}
